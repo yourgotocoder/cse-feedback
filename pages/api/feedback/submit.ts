a2ad97244652cac1e607eb9c65664f8d202491a2
yourgotocoder <yourgotocoder@gmail.com>
2022-10-13 10:52:30 +0530

    Securing the routes

--- pages/api/feedback/submit.ts
+++ pages/api/feedback/submit.ts
@@ -5,23 +5,41 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
   error: boolean;
   message: string;
-  responseId: string;
+  responseId?: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { body } = req;
-  const date = new Date().toLocaleDateString(undefined, {
-    timeZone: "Asia/Kolkata",
-  });
-  const client = await MongoClient.connect(process.env.DB_URL as string);
-  if (client) console.log(`Connected to db`);
-  await client.close();
-  res.status(200).json({
-    error: false,
-    message: "Feedback submitted successfully",
-    responseId: "",
-  });
+  if (req.method === "POST") {
+    const { body } = req;
+    const date = new Date().toLocaleDateString(undefined, {
+      timeZone: "Asia/Kolkata",
+    });
+    try {
+      const client = await MongoClient.connect(process.env.DB_URL as string);
+
+      const db = client.db("feedback-2022");
+      const collection = db.collection("feedback-data");
+      const dataToBeSaved = {
+        ...body,
+        date,
+      };
+      const savedPost = await collection.insertOne(dataToBeSaved);
+      res.status(200).json({
+        error: false,
+        message: "Feedback submitted successfully",
+        responseId: savedPost.insertedId.toString(),
+      });
+      await client.close();
+    } catch (err) {
+      res.status(500).json({
+        error: true,
+        message: "Error connecting to DB",
+      });
+    }
+  } else {
+    res.status(405).json({error: true, message: "Invalid request"})
+  }
 }
