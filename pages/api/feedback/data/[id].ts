058a63ffe5f5a1e7b97f173ea64978a5733d8171
yourgotocoder <yourgotocoder@gmail.com>
2022-10-13 12:37:01 +0530

    API endpoint for retrieving feedback data by ID

---
+++ pages/api/feedback/data/[id].ts
@@ -0,0 +1,44 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MongoAPIError, MongoClient, MongoNotConnectedError, ObjectId } from "mongodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  error: boolean;
+  message: string;
+  data?: any;
+  err?: any;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === "GET") {
+    const { id } = req.query;
+    try {
+      const client = await MongoClient.connect(process.env.DB_URL as string);
+      const db = client.db("feedback-2022");
+      const collection = db.collection("feedback-data");
+      try {
+        const feedbackData = await collection.findOne({
+          _id: new ObjectId(id as string),
+        });
+        await client.close();
+        res
+          .status(200)
+          .json({ error: false, message: "Success", data: feedbackData });
+      } catch (err) {
+        res.status(405).json({ error: true, message: "No result found" });
+      }
+    } catch (err) {
+        console.log(err)
+      res.status(405).json({
+        error: true,
+        message: "Failed to connect to db",
+        err: JSON.stringify(err),
+      });
+    }
+  } else {
+    res.status(405).json({ error: true, message: "Invalid request" });
+  }
+}
